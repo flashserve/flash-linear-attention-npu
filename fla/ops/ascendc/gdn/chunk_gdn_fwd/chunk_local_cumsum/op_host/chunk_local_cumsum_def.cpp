de26ce261ca24df2660254a3b0ba8632cf177aad
fazhenyao <151861644+fazhenyao@users.noreply.github.com>
2026-09-12 18:05:42 +0800

    fix(chunk_local_cumsum): register Ascend950 op config (#536)

--- fla/ops/ascendc/gdn/chunk_gdn_fwd/chunk_local_cumsum/op_host/chunk_local_cumsum_def.cpp
+++ fla/ops/ascendc/gdn/chunk_gdn_fwd/chunk_local_cumsum/op_host/chunk_local_cumsum_def.cpp
@@ -78,11 +78,7 @@ public:
             .ExtendCfgInfo("opFile.value", "chunk_local_cumsum");
         this->AICore().AddConfig("ascend910b", aicoreConfig);
         this->AICore().AddConfig("ascend910_93", aicoreConfig);
-#ifdef ASCEND_SOC_VERSION
-        if (std::string(ASCEND_SOC_VERSION) == "ascend950") {
-            this->AICore().AddConfig("ascend950", aicoreConfig);
-        }
-#endif
+        this->AICore().AddConfig("ascend950", aicoreConfig);
     }
 };
 OP_ADD(ChunkLocalCumsum);
